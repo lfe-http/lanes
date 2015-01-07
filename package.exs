7f6eaf7324f7d2e38de73840b0c4c24c7dddb57f
Duncan M. McGreggor <oubiwann@cogitat.io>
2015-01-06 21:02:04 -0600

    Bumped to version 0.0.3.
    This is in support of the new 'lfetool download deps' command.

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(
   name: "lfest",
   description: "Routing in webapps/services on LFE+YAWS",
-  version: "0.0.2",
+  version: "0.0.3",
   keywords: ["LFE", "Lisp", "Library", "API", "HTTP", "HTML",
              "YAWS", "Routes", "Routing"],
   maintainers: [[name: "Duncan McGreggor", email: "duncan@cogitat.io"]],
